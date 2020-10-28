be5a631172f86e44082208ddd139fe6998665fe5
HolgerZerbe <holger.zerbe@web.de>
2020-10-28 09:36:20 +0100

    005_data_personen created

---
+++ Migrations/005_data_personen.sql
@@ -0,0 +1,4 @@
+USE partnervermittlung;
+INSERT INTO personen (name, vorname, geschlecht, haarfarbe, groesse_in_cm, gewicht_in_kilo, geburtsdatum, sexuelle_orientierung, solvent) VALUES ('Durchschnitt', 'Daniel', 'm', 'keine', 183, 94, '1982-09-30', 'hetero', true);
+INSERT INTO personen (name, vorname, geschlecht, haarfarbe, groesse_in_cm, gewicht_in_kilo, geburtsdatum, sexuelle_orientierung, solvent) VALUES ('Langweilig', 'Lilly', 'f', 'kastanienbraun', 172, 63, '2001-03-31', 'hetero', false);
+INSERT INTO personen (name, vorname, geschlecht, haarfarbe, groesse_in_cm, gewicht_in_kilo, geburtsdatum, sexuelle_orientierung, solvent) VALUES ('Rüstig', 'Rudi', 'd', 'rotblond', 181, 76, '1968-06-28', 'bi', true);
