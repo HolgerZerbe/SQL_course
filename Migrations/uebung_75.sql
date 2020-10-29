49c2fd3b99361a75c3f560631d5f00a95f82cfd9
HolgerZerbe <holger.zerbe@web.de>
2020-10-29 14:08:02 +0100

    uebung 75 finished

--- Migrations/uebung_75.sql
+++ Migrations/uebung_75.sql
@@ -3,3 +3,8 @@ USE fluggesellschaft;
 ALTER TABLE flugzeugtypen CHANGE modell modell VARCHAR(20) NOT NULL;
 ALTER TABLE flugzeugtypen CHANGE hersteller hersteller VARCHAR(50) NOT NULL; 
 ALTER TABLE flugzeugtypen CHANGE maschinenname maschinenname VARCHAR(80) NOT NULL;
+
+ALTER TABLE piloten CHANGE name nachname VARCHAR(50) NOT NULL;
+ALTER TABLE piloten CHANGE vorname vorname VARCHAR(50) NOT NULL;
+ALTER TABLE piloten CHANGE geburtsdatum geburtsdatum DATE NOT NULL;
+ALTER TABLE piloten CHANGE flugstunden flugstunden INT NOT NULL;
