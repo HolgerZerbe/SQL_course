9d61dbb317b40cb912e8cf8f0d9645ec0c97c938
HolgerZerbe <holger.zerbe@web.de>
2020-10-29 11:08:53 +0100

    uebung 71 finished

--- Migrations/uebung_71.sql
+++ Migrations/uebung_71.sql
@@ -1,3 +1,8 @@
+-- 1. Geben Sie alle Filme nach Titel sortiert aus.
+-- 2. Geben Sie alle Filme nach Spieldauer sortiert aus. Dabei soll der längste Film zuerst erscheinen und der kürzeste zuletzt.
+-- 3. Geben Sie alle Filme in der Reihenfolge ihres Erscheinens aus. Falls in einem Jahr mehrere Filme vorhanden sind, so müssen diese innerhalb des Jahres noch alphabetisch sortiert werden.
+
+
 USE filmverleih;
 
 SELECT * FROM filme ORDER BY titel;
