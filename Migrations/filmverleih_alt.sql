41c71c3341abcf1f37b8da8b5da48bb94025adef
HolgerZerbe <holger.zerbe@web.de>
2020-10-30 12:30:53 +0100

    Uebungen 81 und 82 finished

---
+++ Migrations/filmverleih_alt.sql
@@ -0,0 +1,39 @@
+USE filmverleih;
+
+CREATE TABLE filme (id INTEGER PRIMARY KEY AUTO_INCREMENT, titel VARCHAR(150), spieldauer INTEGER, erscheinungsjahr YEAR, kurzbeschreibung TEXT, genre VARCHAR(20), UNIQUE KEY(titel, erscheinungsjahr));
+CREATE TABLE regisseure (id INTEGER PRIMARY KEY AUTO_INCREMENT, name VARCHAR(40) UNIQUE KEY, vorname VARCHAR(40), geburtsdatum DATE);
+ALTER TABLE filme CHANGE erscheinungsjahr erscheinungsjahr YEAR NOT NULL;
+ALTER TABLE regisseure CHANGE vorname vorname VARCHAR(40) NOT NULL;
+ALTER TABLE regisseure CHANGE name name VARCHAR(40) NOT NULL;
+ALTER TABLE filme ADD COLUMN regisseur_id INTEGER;
+ALTER TABLE filme ADD COLUMN preiskategorie_id INTEGER;
+CREATE TABLE preiskategorien (id INTEGER PRIMARY KEY AUTO_INCREMENT, name VARCHAR(15), preis DECIMAL(4, 2));
+
+INSERT INTO filme (titel, erscheinungsjahr, spieldauer, kurzbeschreibung, genre, regisseur_id) VALUES ('Cube', 1997, 90, '7 völlig Fremde mit sehr unterschiedlichen Charakterzügen werden unfreiwillig in ein endloses kafkaartiges Labyrinth voller Fallen gesteckt.', 'scifi', 1);
+INSERT INTO filme (titel, erscheinungsjahr, spieldauer, kurzbeschreibung, genre, regisseur_id) VALUES ('Herr der Ringe - Die Gefährten', 2001, 178, 'In einem kleinen Dorf wird einem junger Hobbit namens Frodo mit ein uralter, magischer Ring anvertraut. Er muss sich auf eine epische Reise zum Schicksaalsberg begeben um ihn zu zerstören.', 'fantasy', 2);
+INSERT INTO filme (titel, erscheinungsjahr, spieldauer, kurzbeschreibung, genre, regisseur_id) VALUES ('Mononoke-hime', 1997, 134, 'Das Schicksaal der Welt liegt in den Händen eines einzigen Kriegers.', 'anime', 3);
+INSERT INTO filme (titel, erscheinungsjahr, spieldauer, kurzbeschreibung, genre, regisseur_id) VALUES ('Donnie Darko', 2001, 113, 'Das Leben ist eine lange verrückte Reise. Einige Leute haben einfach einen besseren Orientierungssinn', 'fantasy', 4);
+INSERT INTO filme (titel, erscheinungsjahr, spieldauer, kurzbeschreibung, genre, regisseur_id) VALUES ('The Dark Knight', 2008, 180, 'Batman, Gordon und Harvey Dent müssen den Joker stoppen, ein Anarchistengenie, der einen Ring der Gewalt über die Stadt legt.', 'action', 5);
+INSERT INTO filme (titel, erscheinungsjahr, spieldauer, kurzbeschreibung, genre, regisseur_id) VALUES ('The Prestige', 2006, 130, 'Robert und Alfred sind Magierrivalen. Als Alfred den ultimativen Trick zeigt, versucht Robert verzweifelt das Geheimnis herauszufinden.', 'fantasy', 5);
+INSERT INTO filme (titel, erscheinungsjahr, spieldauer, kurzbeschreibung, genre, regisseur_id) VALUES ('The Ring', 2002, 115, 'Bevor Du stirbst siehst Du den Ring.', 'horror', 6);
+INSERT INTO filme (titel, erscheinungsjahr, spieldauer, kurzbeschreibung, genre, regisseur_id) VALUES ('The Ring 2', 2005, 110, 'Fear comes full circle.', 'horror', 8);
+INSERT INTO filme (titel, erscheinungsjahr, spieldauer, kurzbeschreibung, genre, regisseur_id) VALUES ('Inglourious Basterds', 2009, 153, 'Es war einmal in einem von Nazis besetzen Frankreich...', 'drama', 7);
+INSERT INTO filme (titel, erscheinungsjahr, spieldauer, kurzbeschreibung, genre, regisseur_id) VALUES ('Star Trek', 2009, 127, 'Die Zukunft beginnt', 'scifi', 9);
+INSERT INTO filme (titel, erscheinungsjahr, spieldauer, kurzbeschreibung, genre, regisseur_id) VALUES ('District 9', 2009, 153, 'Eine auserirdische Rasse ist gezwungen auf der Erde in Slums zu leben...', 'scifi', 10);
+INSERT INTO filme (titel, erscheinungsjahr, spieldauer, kurzbeschreibung, genre, regisseur_id) VALUES ('Fluch der Karibik', 2003, 143, 'Der Schied "Will Turner" verbündet sich mit dem Piraten "Captain" Jack Sparrow" um seine Geliebte zu retten', 'abenteuer', 6);
+INSERT INTO filme (titel, erscheinungsjahr, spieldauer, kurzbeschreibung, genre, regisseur_id) VALUES ('Herr der Ringe - Die Zwei Türme', 2002, 179, 'Frodo & Sam begeben sich nach Mordor um den Ring zu zerstören.', 'fantasy', 2);
+INSERT INTO filme (titel, erscheinungsjahr, spieldauer, kurzbeschreibung, genre, regisseur_id) VALUES ('Braindead', 1992, 104, 'Die Mutter eines Jungen Mannes wird im Zoo gebissen...', 'horror', 2);
+INSERT INTO filme (titel, erscheinungsjahr, spieldauer, kurzbeschreibung, genre, regisseur_id) VALUES ('Pulp Fiction', 1994, 154, 'Die Leben von 2 Auftragsmördern, Einem Boxer und der Frau eines Gangsters vermischen sich..', 'drama', 7);
+
+INSERT INTO regisseure (id, vorname, name, geburtsdatum) VALUES (1,  'Vincent',     'Natali',    '1969-01-06');
+INSERT INTO regisseure (id, vorname, name, geburtsdatum) VALUES (2,  'Peter',       'Jackson',   '1961-10-31');
+INSERT INTO regisseure (id, vorname, name, geburtsdatum) VALUES (3,  'Miyazaki',    'Hayao',     '1951-01-05');
+INSERT INTO regisseure (id, vorname, name, geburtsdatum) VALUES (4,  'Kelly',       'Richard',   '1975-03-28');
+INSERT INTO regisseure (id, vorname, name, geburtsdatum) VALUES (5,  'Christopher', 'Nolan',     '1970-07-30');
+INSERT INTO regisseure (id, vorname, name, geburtsdatum) VALUES (6,  'Gore',        'Verbinski', '1964-03-16');
+INSERT INTO regisseure (id, vorname, name, geburtsdatum) VALUES (7,  'Quentin',     'Tarantino', '1963-03-27');
+INSERT INTO regisseure (id, vorname, name, geburtsdatum) VALUES (8,  'Hideo',       'Nakata',    '1963-03-27');
+INSERT INTO regisseure (id, vorname, name, geburtsdatum) VALUES (9,  'Jeffrey',     'Abrams',    '1966-06-27');
+INSERT INTO regisseure (id, vorname, name, geburtsdatum) VALUES (10, 'Neill',       'Blomkamp',  '1979-09-17');
+
+
---
+++ Migrations/uebung_81.sql
@@ -0,0 +1,3 @@
+USE filmverleih;
+
+CREATE TABLE spielt (film_id INT, schauspieler_id INT, PRIMARY KEY(film_id, schauspieler_id));
